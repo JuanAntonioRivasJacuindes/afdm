d42b2c5cc636bcb5d32f201abb185642b47b4df1
Antonio Jacuindes <arivas@inovul.com>
2022-07-26 16:48:24 -0500

    listo para test

--- routes/web.php
+++ routes/web.php
@@ -62,7 +62,7 @@ Route::post('diploma/update/poster', [DiplomaController::class, 'updatePoster'])
 
 //end Diploma routes
 Route::get('/', function () {
-    dd("funcionandos");
+
     $diplomas = Diploma::where('status_id', 1)->get();
     $courses = Course::all();
     $header = "hola";

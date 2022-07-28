ca35d919d85d182793a01d274d640b47904f9135
Antonio Jacuindes <arivas@inovul.com>
2022-07-28 10:55:22 -0500

    Correccion de bugs visuales en el panel de alumnos

---
+++ resources/views/auxiliar/select-video.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Select Video</title>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <style>
+        body{
+            background-color: #000;
+            color: #fff;
+
+            align-content: center;
+
+            font-family: 'Montserrat';font-size: 8px;
+        }
+        .container{
+
+            width: 100%;
+
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <img style="width: 7rem" src="{{asset('storage/img\afo_logo.png')}}" alt="">
+        <h1><b>POR FAVOR SELECCIONA UN VIDEO DE LA LISTA</b></h1>
+    </div>
+</body>
+</html>

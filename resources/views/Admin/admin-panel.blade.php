583b7c3e08a3af4742936bb732dc31c797841de4
Antonio Jacuindes <arivas@inovul.com>
2022-08-02 10:16:13 -0500

    Cambio visual y responsive del panel admin

--- resources/views/Admin/admin-panel.blade.php
+++ resources/views/Admin/admin-panel.blade.php
@@ -8,28 +8,28 @@
                 <ul class="flex w-full overflow-x-scroll">
                     <li>
                         <a href="#" @click.prevent="currentTab = 1"
-                            :class="currentTab === 1 ? 'border-primary border-4' : 'bg-white'"
+                            :class="currentTab === 1 ? 'bg-gray-200' : 'bg-white'"
                             class="inline-flex justify-center items-center rounded-t py-4 px-6 font-medium transition">
                             Diplomados
                         </a>
                     </li>
                     <li class="ml-1">
                         <a href="#" @click.prevent="currentTab = 2"
-                            :class="currentTab === 2 ? 'border-primary border-4' : 'bg-white'"
+                            :class="currentTab === 2 ? 'bg-gray-200' : 'bg-white'"
                             class="inline-flex justify-center items-center rounded-t py-4 px-6 font-medium transition">
                             Cursos
                         </a>
                     </li>
                     <li class="ml-1">
                         <a href="#" @click.prevent="currentTab = 3"
-                            :class="currentTab === 3 ? 'border-primary border-4' : 'bg-white'"
+                            :class="currentTab === 3 ? 'bg-gray-200' : 'bg-white'"
                             class="inline-flex justify-center items-center rounded-t py-4 px-6 font-medium transition">
                             Usuarios
                         </a>
                     </li>
                     <li class="ml-1">
                         <a href="#" @click.prevent="currentTab = 4"
-                            :class="currentTab === 4 ? 'border-primary border-4' : 'bg-white'"
+                            :class="currentTab === 4 ? 'bg-gray-200' : 'bg-white'"
                             class="inline-flex justify-center items-center rounded-t py-4 px-6 font-medium transition">
                             Roles y permisos
                         </a>

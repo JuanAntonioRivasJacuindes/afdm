bbc244fbf53df0d3cc8fa47c091446bc805c2331
Antonio Jacuindes <arivas@inovul.com>
2022-08-11 10:30:14 -0500

    Intervencion en la migracion de planes

--- resources/views/livewire/subproduct/show.blade.php
+++ resources/views/livewire/subproduct/show.blade.php
@@ -1,3 +1,8 @@
 <x-app-layout>
+    <div class="w-full m-5 p-5 bg-white">
+        <header>{{$subproduct->name}}</header>
+        <h2>{{$subproduct->description ??'No Definido'}}</h2>
+        <h2>{{$subproduct->stipe_id??'no definido'}}</h2>
+    </div>
 
 </x-app-layout>
